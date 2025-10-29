e37b3d17ddb9a2908a2a4dd02b222c73a8ba8a9d
joaobent <joaopedro0744@hotmail.com>
2025-10-29 18:32:19 -0400

    up

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,35 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import Image from 'next/image';
+import styles from './page.module.css';
+import { Sparkles } from 'lucide-react';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>Pagina inicial</h1>
-      </main>
-    </div>
+    <main className={styles.hero}>
+      <div className={styles.container}>
+        <div className={styles.info}>
+          <div className={styles.ia}>
+            <Sparkles />
+            <p>Finanças Inteligentes com IA</p>
+          </div>
+          <div className={styles.titulo}>
+            <h1>Controle suas finanças com inteligência artificial</h1>
+          </div>
+          <div className={styles.text}>
+            <h3>Registre suas despesas, receba insights personalizados e economize dinheiro de forma inteligente.
+              A IA analisa seus gastos e sugere as melhores estratégias financeiras.</h3>
+          </div>
+          <div className={styles.botoes}>
+            <button>Começar Gratuitamente</button>
+            <button>Ver Demonstração</button>
+          </div>
+        </div>
+
+        <div className={styles.divImage}>
+          <img src="/images/financia.png" alt="" />
+        </div>
+      </div>
+    </main>
   );
 }
